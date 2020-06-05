29ad47fe2c92a3c54d8df64b67dbe9b6a0c16832
Ofek Shochat <50481015+OfekShochat@users.noreply.github.com>
2020-06-05 13:26:55 +0300

    Update train example.py

--- examples/train example.py
+++ examples/train example.py
@@ -1,4 +1,4 @@
-from neuralnet import train
+from lib import train
 test = train(number_of_train = 40000000, b = 0, target = 498.0034275470891, xs = [3.6, 8, 4])
 test.ws = test.defaultws()
 test.train()
